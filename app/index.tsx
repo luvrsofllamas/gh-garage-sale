c1aa8f4d547dd83b8c4aff3c6f1814761c473fa3
Keith Kurak <keith.kurak@gmail.com>
2024-06-20 00:11:57 -0400

    google maps working

--- app/index.tsx
+++ app/index.tsx
@@ -1,88 +1,205 @@
-import { useState } from "react";
-import { Text, ScrollView, View, Pressable } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  Text,
+  ScrollView,
+  View,
+  Pressable,
+  useWindowDimensions,
+} from "react-native";
 import { Stack } from "expo-router";
 import sales from "@/constants/sales.json";
+import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
+import CheckBox from "expo-checkbox";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const SegmentedControl = (
+  {
+    items,
+    onSetItemIndex,
+    selectedIndex,
+  } /*: { items: { title: string, color: string}[], onSetItemIndex: number => void }*/
+) => {
+  return (
+    <View
+      style={{
+        marginTop: 10,
+        marginBottom: 10,
+        marginHorizontal: 50,
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+    >
+      {items.map((item, index) => (
+        <Pressable
+          key={item.title}
+          onPress={() => onSetItemIndex(index)}
+          style={{
+            backgroundColor:
+              selectedIndex === index ? item.color : "transparent",
+            padding: 10,
+            paddingHorizontal: 20,
+            borderColor: item.color,
+            borderWidth: 1,
+          }}
+        >
+          <Text
+            style={{
+              color: selectedIndex === index ? "white" : item.color,
+            }}
+          >
+            {item.title}
+          </Text>
+        </Pressable>
+      ))}
+    </View>
+  );
+};
 
 export default function HomeScreen() {
-  const [selectedDateRange, setSelectedDateRange] =
-    useState("fridayAndSaturday");
+  const [filterIndex, setFilterIndex] = useState(2);
+
+  const [selectedItem, setSelectedItem] = useState<string | undefined>(
+    undefined
+  );
+
+  const [selectedSales, setSelectedSales] = useState<any>({});
+
+  const windowDimensions = useWindowDimensions();
+
+  // clear selected sale if not in list
+  useEffect(() => {
+    if (selectedItem && !sales.find((sale) => sale.address === selectedItem)) {
+      setSelectedItem(undefined);
+    }
+  }, [selectedItem]);
+
+  // load selected sales from local storage
+  useEffect(() => {
+    AsyncStorage.getItem("selectedSales").then((value) => {
+      if (!value) return;
+      setSelectedSales(JSON.parse(value));
+    });
+  }, []);
+
+  // save selected sales to local storage
+  useEffect(() => {
+    AsyncStorage.setItem("selectedSales", JSON.stringify(selectedSales));
+  }, [selectedSales]);
 
   const filteredSales =
-    selectedDateRange === "fridayAndSaturday"
-      ? sales.filter((sale) => sale.saturday && sale.friday)
-      : sales.filter((sale) => !sale.friday);
+    filterIndex === 0
+      ? sales.filter((sale) => sale.friday)
+      : filterIndex === 1
+      ? sales.filter((sale) => sale.saturday)
+      : sales; // TODO: sort by street name
+
+  const filterItems = [
+    { title: "Friday", color: "blue" },
+    { title: "Saturday", color: "green" },
+    { title: "All", color: "purple" },
+  ];
 
   return (
-    <ScrollView
-      contentContainerStyle={{ flex: 1, marginHorizontal: 20, marginTop: 10 }}
-    >
-      <Stack.Screen options={{ title: "South Hills Garage Sales" }} />
-      <View style={{ width: "100%", height: 400 }}>
-        <iframe
-          src="https://app.mapline.com/map/map_73ea4855/UVNuVWh4bnVCbkNjcXhKSWowT05iMmRuNkQ2S01NNE42c0V1Rm"
-          style={{ width: "100%", height: 400 }}
-          allow="geolocation *"
-        ></iframe>
+    <View style={{ flex: 1 }}>
+      <View style={{ width: "100%", height: windowDimensions.height / 2 }}>
+        <APIProvider apiKey={process.env.EXPO_PUBLIC_MAPS_API_KEY as string}>
+          <Map
+            mapId={"c1d957b78da88177"}
+            style={{ width: "100%", height: "100%" }}
+            defaultCenter={{ lat: 41.43163317037717, lng: -81.69252125368092 }}
+            defaultZoom={15.2}
+            gestureHandling={"greedy"}
+            disableDefaultUI={true}
+          >
+            {filteredSales.map((sale) => (
+              <AdvancedMarker key={sale.address} position={sale.coords}>
+                <View
+                  style={{
+                    backgroundColor: filterItems[filterIndex].color,
+                    height: 16,
+                    width: 16,
+                    borderRadius: 8,
+                  }}
+                ></View>
+              </AdvancedMarker>
+            ))}
+            {selectedItem && (
+              <AdvancedMarker
+                key={"selected"}
+                position={
+                  sales.find((sale) => sale.address === selectedItem)?.coords
+                }
+              >
+                <View
+                  style={{
+                    backgroundColor: "yellow",
+                    height: 24,
+                    width: 24,
+                    borderRadius: 12,
+                    borderWidth: 6,
+                    borderColor: filterItems[filterIndex].color,
+                  }}
+                ></View>
+              </AdvancedMarker>
+            )}
+          </Map>
+        </APIProvider>
       </View>
-      <View
-        style={{
-          marginTop: 10,
-          marginBottom: 10,
-          marginHorizontal: 50,
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
+      <ScrollView
+        contentContainerStyle={{ flex: 1, marginHorizontal: 20, marginTop: 10 }}
       >
-        <Pressable
-          onPress={() => setSelectedDateRange("saturdayOnly")}
-          style={{
-            backgroundColor:
-              selectedDateRange === "saturdayOnly" ? "blue" : "transparent",
-            padding: 10,
-            borderColor: "blue",
-            borderWidth: 1,
-          }}
-        >
-          <Text
-            style={{
-              color: selectedDateRange === "saturdayOnly" ? "white" : "black",
-            }}
-          >
-            Saturday Only
-          </Text>
-        </Pressable>
-        <Pressable
-          onPress={() => setSelectedDateRange("fridayAndSaturday")}
-          style={{
-            backgroundColor:
-              selectedDateRange === "fridayAndSaturday"
-                ? "green"
-                : "transparent",
-            borderColor: "green",
-            borderWidth: 1,
-            padding: 10,
-          }}
-        >
-          <Text
-            style={{
-              color:
-                selectedDateRange === "fridayAndSaturday" ? "white" : "black",
-            }}
-          >
-            Friday and Saturday
-          </Text>
-        </Pressable>
-      </View>
-      <View style={{ marginTop: 10, rowGap: 10 }}>
-        {filteredSales.map((sale) => (
-          <Text
-            key={sale.address}
-            style={{ fontSize: 18, textAlign: "center" }}
-          >
-            {sale.address}
-          </Text>
-        ))}
-      </View>
-    </ScrollView>
+        <Stack.Screen options={{ title: "South Hills Garage Sales" }} />
+        <SegmentedControl
+          items={filterItems}
+          onSetItemIndex={setFilterIndex}
+          selectedIndex={filterIndex}
+        />
+        <View style={{ marginTop: 10, rowGap: 5 }}>
+          {filteredSales.map((sale) => (
+            <View
+              key={sale.address}
+              style={{
+                flexDirection: "row",
+                paddingLeft: 10,
+                alignItems: "center",
+                height: 40,
+                backgroundColor:
+                  selectedItem === sale.address ? "yellow" : undefined,
+              }}
+            >
+              <CheckBox
+                value={!!selectedSales[sale.address]}
+                onValueChange={(checked) =>
+                  setSelectedSales({
+                    ...selectedSales,
+                    [sale.address]: checked,
+                  })
+                }
+              />
+              <Pressable style={{ flex: 1 }} onPress={() => setSelectedItem(sale.address)}>
+                <View style={{ flex: 1, flexDirection: "row" }}>
+                  <Text style={{ fontSize: 20, paddingLeft: 10 }}>
+                    {sale.address}
+                  </Text>
+
+                  {!sale.friday && sale.saturday && (
+                    <Text
+                      style={{
+                        fontSize: 12,
+                        paddingLeft: 8,
+                        color: "green",
+                        alignSelf: "flex-start",
+                      }}
+                    >
+                      SAT ONLY
+                    </Text>
+                  )}
+                </View>
+              </Pressable>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
   );
 }
